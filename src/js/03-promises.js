8739c976c3318f5baf63e280e61d5c40a53d62c1
Bartek Świderski <bartekzg@gmail.com>
2021-09-12 19:44:48 +0200

    zad 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,27 @@
+import Notiflix from "../../node_modules/notiflix";
+const qs = (selector) => document.querySelector(selector);
+const form = qs(".form")
 function createPromise(position, delay) {
+  setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+},delay)
 }
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount}
+  } = event.currentTarget;
+for(let i =0; i< amount.value; i++){
+ let repet = i+1
+ let del = Number(delay.value)+Number(i*step.value);
+  createPromise(repet,del)
+}
+
+  event.currentTarget.reset();
+}
+form.addEventListener("submit", handleSubmit);
